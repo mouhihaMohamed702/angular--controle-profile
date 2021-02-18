744fc4eb236ed2528b061753c86f35484c36b926
mouhihaMohamed702 <mouhihamohamed@gmail.com>
2021-02-18 22:44:03 +0100

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AdminComponent } from './layouts/admin/admin.component';
+
+const routes: Routes = [
+  
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch: 'full',
+
+  },
+ 
+  {
+    path:'', component:AdminComponent,
+       children: [
+      {
+        path: '',
+        loadChildren: () => import('./layouts/admin/admin.module').then(m => m.AdminModule)
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:'/home'
+  }
+
+  
+ 
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,  { enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Section1Component } from './section1/section1.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { AdminComponent } from './layouts/admin/admin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NavbarComponent,
+    Section1Component,
+    FooterComponent,
+   
+    AdminComponent,
+   
   ],
   imports: [
     BrowserModule,
